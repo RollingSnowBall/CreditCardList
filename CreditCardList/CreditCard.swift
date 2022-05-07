b729c48ecf2c6cba52df3114c09ad78286fd3b26
JUNO <jrlee0701@kakao.com>
2022-05-08 00:23:10 +0900

    기본 UI 구성

---
+++ CreditCardList/CreditCard.swift
@@ -0,0 +1,27 @@
+//
+//  CreditCard.swift
+//  CreditCardList
+//
+//  Created by JUNO on 2022/05/07.
+//
+
+import Foundation
+
+struct CreditCard: Codable {
+    let id: Int
+    let rank: Int
+    let name: String
+    let cardImageURL: String
+    let promotionDetail: PromotionDetail
+    let isSelected: Bool?
+}
+
+struct PromotionDetail: Codable {
+    let companyName: String
+    let amount: Int
+    let benefitDate: String
+    let benefitDetail: String
+    let benefitCondition: String
+    let condidtion: String
+    let period: String
+}
